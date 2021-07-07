ace64fb43b920c53bfb93bcaf1aa89c8d43f1fba
jokojiro <jokojiro@yahoo.co.jp>
2021-07-07 19:05:08 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'prototypes#index'
-  resources :prototypes do
-    resources :comments, only: :create
+  root to: "prototypes#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :users, only: :show
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
